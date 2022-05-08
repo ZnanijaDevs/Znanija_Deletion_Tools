8cd3c9beb4a729ef7c34506789de283df06776e8
southpaw203 <73884858+vlaex@users.noreply.github.com>
2022-05-08 19:28:03 +0300

    Update App.tsx

--- src/views/AccountsQueue/App.tsx
+++ src/views/AccountsQueue/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { runtime } from "webextension-polyfill";
 import { Flex, TextBit, Spinner } from "brainly-style-guide";
 
@@ -9,44 +9,69 @@ import ActiveUsers from "./components/ActiveUsers";
 import QueueItem from "./components/QueueItem";
 import ErrorContainer from "./components/ErrorContainer";
 
-export default function App() {
-  const [queue, setQueue] = useState<Message[]>([]);
-  const [error, setError] = useState("");
-  const [activeUsers, setActiveUsers] = useState<ActiveUser[]>([]);
-  const [connected, setConnected] = useState(false);
+type AppStateDataType = {
+  queue: Message[];
+  error: string;
+  activeUsers: ActiveUser[];
+  connected: boolean;
+}
 
-  useEffect(() => {
-    new LiveWebSocket().Open().then(ws => {
-      ws
-        .onClose(reason => {
-          setError(reason);
-          setConnected(false);
-        })
-        .onEvent("hello_there", _ => setConnected(true))
-        .onEvent("messages_updated", data => setQueue(data.messages))
-        .onEvent("users_updated", users => setActiveUsers(users));
-    });
-  }, []);
+export default class App extends React.Component {
+  state: AppStateDataType = {
+    queue: [],
+    error: "",
+    activeUsers: [],
+    connected: false
+  };
 
-  if (error) {
-    return <ErrorContainer error={error} />;
-  } else if (!connected) {
-    return <Spinner />;
-  } else if (!queue.length) {
-    return <img draggable="false" height="300" title="Нет сообщений" src={runtime.getURL("vzhuh.jpg")} />;
+  componentDidMount() {
+    this.OpenConnection();
   }
 
-  return (
-    <Flex fullWidth fullHeight direction="column">
-      <Flex justifyContent="space-between" className="queue__header" alignItems="center">
-        <TextBit>#to-delete [{queue.length}]</TextBit>
-        <ActiveUsers users={activeUsers} />
+  async OpenConnection() {
+    const ws = await new LiveWebSocket().Open();
+
+    ws
+      .onClose(reason => 
+        this.setState({
+          error: reason,
+          connected: false
+        })
+      )
+      .onEvent("hello_there", _ =>
+        this.setState({ connected: true })
+      )
+      .onEvent("messages_updated", data => 
+        this.setState({ queue: data.messages })
+      )
+      .onEvent("users_updated", users =>
+        this.setState({ activeUsers: users })
+      );
+  }
+
+  render() {
+    const { error, connected, queue, activeUsers } = this.state;
+
+    if (error) {
+      return <ErrorContainer error={error} />;
+    } else if (!connected) {
+      return <Spinner />;
+    } else if (!queue.length) {
+      return <img draggable="false" height="300" title="Нет сообщений" src={runtime.getURL("vzhuh.jpg")} />;
+    }
+  
+    return (
+      <Flex fullWidth fullHeight direction="column">
+        <Flex justifyContent="space-between" className="queue__header" alignItems="center">
+          <TextBit>#to-delete [{queue.length}]</TextBit>
+          <ActiveUsers users={activeUsers} />
+        </Flex>
+        <Flex direction="column" className="queue__items">
+          {queue.map((item, i) => 
+            <QueueItem key={i} item={item} />
+          )}
+        </Flex>
       </Flex>
-      <Flex direction="column" className="queue__items">
-        {queue.map((item, i) => 
-          <QueueItem key={i} item={item} />
-        )}
-      </Flex>
-    </Flex>
-  );
+    );
+  }
 }
